557396429509c7cf470352925bbe4efb2e396bf6
Adnane BC <adnane.bachchar@gmail.com>
2026-04-05 01:56:48 +0100

    first exercise done

---
+++ exercices/day-3.js
@@ -0,0 +1,19 @@
+// 1
+//Check if number is even
+if (number % 2 === 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
+// 2
+// Find largest of 3 numbers
+
+
+// 3
+Print numbers 1 to 50
+
+// 4
+Sum numbers 1 to 100
+
+// 5
+Count vowels in string
